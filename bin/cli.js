baed48d4ffc30d19e230294f07e7282d4ecfbf66
hex7c0 <carniellifrancesco@gmail.com>
2014-12-27 10:17:12 +0100

    try catch logic

--- bin/cli.js
+++ bin/cli.js
@@ -17,31 +17,40 @@ cli
       collections: [ 'c', 'Specifies a collection to backup', 'string' ],
       query: [ 'q', 'Query that optionally limits the documents included',
           'string' ],
+      metadata: [ 'm', 'Save metadata of collections as Index, ecc' ],
       version: [ 'v', 'Display the current version' ]
     });
 
-cli.main(function(args, options) {
+cli
+    .main(function(args, options) {
 
-  var self = this;
-  if (options.version) {
-    return console.log(process.title + ' v' + VERSION);
-  } else if (args.length == 0 && !options.host) {
-    self.fatal('Missing uri option');
-  }
+      var self = this;
+      if (options.version) {
+        return console.log(process.title + ' v' + VERSION);
+      } else if (args.length == 0 && !options.host) {
+        self.fatal('Missing uri option');
+      }
 
-  self.spinner('Working..');
-  var backup = require('mongodb-backup');
-  backup({
-    uri: args[0] || options.host,
-    root: options.out,
-    parser: options.parser,
-    collections: options.collections ? JSON.parse(options.collections) : null,
-    tar: options.tar,
-    query: options.query ? JSON.parse(options.query) : null,
-    logger: options.verbose,
-    callback: function() {
+      self.spinner('Working..');
+      var backup = require('mongodb-backup');
+      try {
+        backup({
+          uri: args[0] || options.host,
+          root: options.out,
+          parser: options.parser,
+          collections: options.collections ? JSON.parse(options.collections)
+              : null,
+          tar: options.tar,
+          query: options.query ? JSON.parse(options.query) : null,
+          logger: options.verbose,
+          metadata: options.metadata,
+          callback: function() {
 
-      self.spinner('Working.. done\n', true);
-    }
-  });
-});
+            self.spinner('Working.. done\n', true);
+          }
+        });
+      } catch (e) {
+        self.spinner('Working.. error\n', true);
+        console.error(e.message);
+      }
+    });
